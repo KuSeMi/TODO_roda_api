7460f7436136893bbeed9e3b23d1b106b1117ef5
serhii.kushnir <serhii.kushnir@cd.p2h.com>
2023-01-30 16:45:39 +0200

    dry validation

---
+++ lib/exceptions.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+# {Exceptions} module defines errors classes that are used in application.
+module Exceptions
+  # {Exceptions::InvalidParamsError} is an error which is raised when invalid params are passed to the endpoint.
+  class InvalidParamsError < StandardError
+    attr_reader :object
+
+    # @param [Hash] object that contains details about params errors.
+    # @param [String] message of the error.
+    def initialize(object, message)
+      @object = object
+      super(message)
+    end
+  end
+end
